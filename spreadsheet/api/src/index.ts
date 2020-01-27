6bb4fad6caf9fd78a3c96df5e22ec6dd92e0005d
Alexis Mineaud <alexis.mineaud@alsid.com>
2020-01-27 11:30:29 +0100

    Start API implementation.

---
+++ spreadsheet/api/src/index.ts
@@ -0,0 +1,93 @@
+import { Maybe } from 'types/helpers'
+import { assertUnreachable } from './libs/asserts'
+
+// tslint:disable:no-eval
+// tslint:disable:max-classes-per-file
+
+interface IGridCellCoord {
+  column: string
+  row: number
+}
+
+type GridCellCoord = string
+type GridCellValue = string
+
+enum GridCellType {
+  string = 'string',
+  number = 'number'
+}
+
+function buildGridCellCoord(gridCellCoordObj: IGridCellCoord): GridCellCoord {
+  return [gridCellCoordObj.column, gridCellCoordObj.row].join(':')
+}
+
+class GridCell {
+  constructor(private $value: Maybe<string>, private $type: GridCellType) {}
+
+  getValue(): GridCellValue {
+    switch (this.$type) {
+      case GridCellType.string:
+        return this.$value || ''
+
+      case GridCellType.number:
+        return String(Number(this.$value || 0))
+
+      default:
+        assertUnreachable(this.$type)
+    }
+  }
+}
+
+class Grid {
+  private $grid: Map<GridCellCoord, GridCell> = new Map()
+
+  setGridCell(gridCellCoordObj: IGridCellCoord, gridCell: GridCell): void {
+    this.$grid.set(buildGridCellCoord(gridCellCoordObj), gridCell)
+  }
+
+  getGridCell(gridCellCoordObj: IGridCellCoord): GridCell {
+    return (
+      this.$grid.get(buildGridCellCoord(gridCellCoordObj)) ||
+      new GridCell(null, GridCellType.string)
+    )
+  }
+
+  getCellValue(cellCoordObj: IGridCellCoord): string | number {
+    const cellGrid = this.getGridCell(cellCoordObj)
+    return cellGrid.getValue()
+  }
+}
+
+const grid = new Grid()
+
+grid.setGridCell(
+  { column: 'A', row: 1 },
+  new GridCell('10', GridCellType.number)
+)
+
+const cases = [
+  // 42
+  eval(`(new GridCell('42', GridCellType.number)).getValue()`),
+
+  // 'foo'
+  eval(`(new GridCell('foo', GridCellType.number)).getValue()`),
+
+  // =$A1
+  eval(`grid.getCellValue({ column: 'A', row: 1 })`),
+
+  // =$A1+42
+  eval(
+    `grid.getCellValue({ column: 'A', row: 1 }) + (new GridCell('42', GridCellType.number)).getValue()`
+  )
+
+  // =$A1+$A2
+  // eval(grid.getCell('A', 1) + grid.getCell('A', 2))
+
+  // =($A1+$A2)/$B3
+  // eval((grid.getCell('A', 1) + grid.getCell('A', 2)) / grid.getCell('B', 3))
+
+  // =`Math.round($A1)`
+  // eval(Math.round(grid.getCell('A', 1))
+].forEach(result => {
+  console.log('result?', result)
+})
---
+++ spreadsheet/api/src/libs/asserts.ts
@@ -0,0 +1,9 @@
+import { Maybe } from 'types/helpers'
+
+/**
+ * Allow to implement all cases of a switch statement.
+ * Credit: https://stackoverflow.com/a/39419171/2743091
+ */
+export function assertUnreachable(x: Maybe<never>): never {
+  throw new Error(`Didn't expect to get here (${x})`)
+}
